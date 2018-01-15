2e5c77da68a1cc5e0271d20d9c933e491b837b12
Zoratoostar <toevgeny@gmail.com>
2018-01-16 01:18:56 +0800

    initial migrations

---
+++ db/migrate/20180115135607_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[5.1]
+  def change
+    create_table :questions do |t|
+      t.text :body
+      t.string :right_option
+      t.integer :test_id
+
+      t.timestamps
+    end
+  end
+end
