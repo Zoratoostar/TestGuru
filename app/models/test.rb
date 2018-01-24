6fe610d479d0c5021bbae3e50d8105ca683c55f7
Zoratoostar <toevgeny@gmail.com>
2018-01-25 02:22:17 +0800

    Add some methods

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,7 @@
 class Test < ApplicationRecord
+  def self.titles_of(category_name)
+    category = Category.where(title: category_name).first
+    return [] unless category
+    self.where(category_id: category.id).pluck(:title).sort { |x, y| y <=> x }
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def touched_tests(level)
+    test_ids = Evaluation.where(user_id: self.id).pluck(:test_id).uniq
+    Test.where(level: level, id: test_ids)
+  end
 end
