7a7fcb3548c42b5056f7f096f964ffd2c9f4c7ff
Zoratoostar <toevgeny@gmail.com>
2018-01-26 19:08:15 +0800

    Optimize sorting

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,6 @@ class Test < ApplicationRecord
   def self.titles_of(category_name)
     category = Category.where(title: category_name).first
     return [] unless category
-    self.where(category_id: category.id).pluck(:title).sort { |x, y| y <=> x }
+    self.where(category_id: category.id).order(title: :DESC).pluck(:title)
   end
 end
