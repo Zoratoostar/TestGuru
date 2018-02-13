da371db926ca293080160990aeeec19d3dd64364
Evgeny <toevgeny@gmail.com>
2018-02-13 21:03:12 +0800

    renamed Evaluation to TestEvaluation

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :questions, through: :answers
 
   def touched_tests(level)
-    test_ids = Evaluation.where(user_id: self.id).pluck(:test_id).uniq
+    test_ids = TestEvaluation.where(user_id: self.id).pluck(:test_id).uniq
     Test.where(level: level, id: test_ids)
   end
 end
