7733566a8b0042b5d1db7c158da398cd87bacf07
Zoratoostar <toevgeny@gmail.com>
2018-03-10 02:52:01 +0800

    add helpers and footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def current_year
+    Time.now.to_formatted_s(:year)
+  end
+
+  def github_url(author, repo)
+    link_to repo, "https://github.com/#{author}/#{repo}"
+  end
+
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,7 +1,7 @@
 module QuestionsHelper
 
-  def question_header(question)
-    header = "#{question.test.title} Question"
+  def question_header(test_id)
+    header = "#{Test.find(test_id).title} Question"
     reqfp = request.fullpath
     header = "Create New #{header}" if reqfp.include?('new')
     header = "Edit #{header}" if reqfp.include?('edit')
